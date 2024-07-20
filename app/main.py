09386c82cd58ec4eaa27d69394d472be05125d63
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-07-21 00:36:21 +0400

    Iteration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import asyncio
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-import time
 
 app = FastAPI()
 
@@ -15,16 +15,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 async def generate_text():
-    words = "This is a long hardcoded text that will be streamed to the UI.".split()
-    for word in words:
-        yield word + " "  # Add a space after each word
+    sentence = "This is a long hardcoded text that will be streamed to the UI."
+    for index in range(0, len(sentence), 5):
+        yield sentence[index:index+5]
         await asyncio.sleep(0.05)  # 0.5 second delay
 
-@app.get("/api/stream")
-async def stream_text():
+
+@app.post("/api/chat")
+async def stream_text(body: dict):
+    print(body)
     return StreamingResponse(generate_text(), media_type="text/plain")
 
-@app.get("/api/")
+
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
